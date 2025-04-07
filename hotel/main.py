bc17c36a0f2c682d73b64fd728588fe0d37c6d32
slerbu <lindgrla@arcada.fi>
2025-04-07 10:11:19 +0000

    hotelapi stuff, add dotenv and conn

--- hotel/main.py
+++ hotel/main.py
@@ -1,21 +1,51 @@
 import uvicorn
+import psycopg
+import os
+from psycopg.rows import dict_row
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
+
 PORT=8282
+
+# Load environment variables
+load_dotenv()
+DB_URL = os.getenv("DB_URL")
+print(DB_URL)
+#Create DB connection
+conn = psycopg.connect(DB_URL, autocommit = True, row_factory = dict_row)
+
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
+@app.get("/temp")
+def temp():
+    return {"msg": "Hello"}
+
 rooms = [
-    {"number": 513, "type": "single"},
-    {"number": 312, "type": "double"},
-    {"number": 123, "type": "suite"}
+    {"number": 513, "type": "single", "price": 99},
+    {"number": 312, "type": "double", "price:": 199},
+    {"number": 123, "type": "suite", "price": 499}
 ]
 @app.get("/rooms") 
-def getRooms(request: Request):
-    
+def get_rooms():
+   
     return rooms
+
+#Get one room
+
+@app.get("/rooms/{id}") 
+def get_one_room(id: int):
+    try:
+        return rooms[id]
+    except:
+        return { "error": "Room not found"}
+
+@app.post("/bookings")
+def create_booking(request: Request):
+    return {"msg": "booking created!"}
     
 
 if __name__ == "__main__":
