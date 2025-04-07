e0f778856cabcd0b3e37a8626d65cfbc99559ae8
slerbu <lindgrla@arcada.fi>
2025-04-07 11:42:49 +0000

    hotel stuff bookings

--- hotel/main.py
+++ hotel/main.py
@@ -5,6 +5,8 @@ from psycopg.rows import dict_row
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from datetime import date
 
 
 PORT=8282
@@ -20,6 +22,14 @@ conn = psycopg.connect(DB_URL, autocommit = True, row_factory = dict_row)
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
+#datamodell som ska valideras
+class Booking(BaseModel):
+    guest_id:int
+    room_id:int
+    datefrom: date
+    dateto: date
+
+
 @app.get("/temp")
 def temp():
     with conn.cursor() as cur:
@@ -34,21 +44,59 @@ rooms = [
 ]
 @app.get("/rooms") 
 def get_rooms():
-   
-    return rooms
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM hotel_rooms ORDER BY room_number")
+        rooms = cur.fetchall()
+        return rooms
+
+
 
 #Get one room
 
 @app.get("/rooms/{id}") 
 def get_one_room(id: int):
-    try:
-        return rooms[id]
-    except:
-        return { "error": "Room not found"}
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM hotel_rooms WHERE id = %s", [id])
+        room = cur.fetchone()
+        if not room:
+            return {"msg": "Room not found"}
+        return room
+
+@app.get("/bookings")
+def get_bookings():
+    with conn.cursor() as cur:
+        cur.execute("""
+            SELECT * 
+            FROM hotel_bookings
+            ORDER BY datefrom""")
+        bookings = cur.fetchall()
+        return bookings
+
 
 @app.post("/bookings")
-def create_booking(request: Request):
-    return {"msg": "booking created!"}
+def create_booking(booking: Booking):
+    with conn.cursor() as cur:
+        cur.execute("""INSERT INTO hotel_bookings (
+            guest_id,
+            room_id,
+            datefrom,
+            dateto
+        ) VALUES (
+            %s,
+            %s,
+            %s,
+            %s        
+        )
+        RETURNING id
+        """, [
+            booking.guest_id,
+            booking.room_id, 
+            booking.datefrom, 
+            booking.dateto
+        ])
+        new_id=cur.fetchone()['id']
+    return {"msg": "booking created!", "id": new_id}
+
     
 
 if __name__ == "__main__":
